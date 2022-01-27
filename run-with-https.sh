6be267eaa1249b569a83f3c7aada36685fcb2a5d
Aram Peres <aram.peres@wavy.fm>
2022-01-27 01:17:01 -0500

    Force certbot renewal on start

--- run-with-https.sh
+++ run-with-https.sh
@@ -5,7 +5,7 @@ then
   export PORT=443
   export HTTPS=1
   certbot certonly --standalone --preferred-challenges http -d status.wavy.fm \
-    --non-interactive --agree-tos -m aram.peres@wavy.fm --post-hook "/opt/status-page/wavy-status-page"
+    --non-interactive --agree-tos -m aram.peres@wavy.fm --force-renewal --post-hook "/opt/status-page/wavy-status-page"
 else
   /opt/status-page/wavy-status-page
 fi
